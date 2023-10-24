526395dc8d8358f4a20baf342917d6bb1ff21b0f
Fiaz Hussain <fiaz.hussain@ceative.co.uk>
2023-10-24 07:08:00 +0000

    Squashed commit of the following
    commit 1cdd580b 
    Author: Fiaz Hussain <fiaz.hussain@ceative.co.uk> 
    Date: Thu Oct 19 2023 12:34:40 GMT+0500 (Pakistan Standard Time) 
    
        refactor(add auth): add auth userId in news and events module (#AACSA-136)

--- apps/gateway/src/app/controllers/super-admin/news-and-events.contoller.ts
+++ apps/gateway/src/app/controllers/super-admin/news-and-events.contoller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  Req,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import {
@@ -36,6 +37,8 @@ import {
   IdDto,
 } from '@shared/dto';
 import { firstValueFrom } from 'rxjs';
+import { AppRequest } from '../../shared/interface/request.interface';
+import { Auth } from '../../decorators/auth.decorator';
 
 @ApiBearerAuth()
 @ApiTags(API_TAGS.NEWS_AND_EVENTS)
@@ -46,12 +49,14 @@ export class NewsAndEventsController {
     private superAdminService: ClientProxy
   ) {}
 
+  @Auth(true)
   @Post(API_ENDPOINTS.NEWS_AND_EVENTS.Add_NEWS_OR_EVENT)
   @ApiCreatedResponse({ type: AddNewsOrEventResponseDto })
   public async addNewsOrEvent(
+    @Req() request: AppRequest,
     @Body() payload: AddNewsOrEventDto
   ): Promise<AddNewsOrEventResponseDto> {
-    payload.createdBy = '56cb91bdc3464f14678934ca'; // TODO get user id from token data
+    payload.createdBy = request?.user?._id;
     const response = await firstValueFrom(
       this.superAdminService.send(
         RMQ_MESSAGES.NEWS_AND_EVENTS.ADD_NEWS_AND_EVENTS,
@@ -62,6 +67,7 @@ export class NewsAndEventsController {
     return response;
   }
 
+  @Auth(true)
   @Get(API_ENDPOINTS.NEWS_AND_EVENTS.GET_NEWS_OR_EVENTS)
   @ApiOkResponse({ type: GetNewsAndEventsResponseDto })
   public async getNewsOrEvents(
@@ -77,6 +83,7 @@ export class NewsAndEventsController {
     return response;
   }
 
+  @Auth(true)
   @Get(API_ENDPOINTS.NEWS_AND_EVENTS.GET_NEWS_OR_EVENT)
   @ApiOkResponse({ type: GetNewsOrEventResponseDto })
   public async getNewsOrEvent(
@@ -92,13 +99,15 @@ export class NewsAndEventsController {
     return response;
   }
 
+  @Auth(true)
   @Patch(API_ENDPOINTS.NEWS_AND_EVENTS.EDIT_NEWS_OR_EVENT)
   @ApiOkResponse({ type: EditNewsOrEventResponseDto })
   public async editNewsOrEvent(
+    @Req() request: AppRequest,
     @Param() params: IdDto,
     @Body() payload: EditNewsOrEventDto
   ): Promise<EditNewsOrEventResponseDto> {
-    payload.updatedBy = '56cb91bdc3464f14678934ca'; // TODO get user id from token data
+    payload.updatedBy = request?.user?._id;
     payload.id = params.id;
 
     const response = await firstValueFrom(
@@ -111,11 +120,13 @@ export class NewsAndEventsController {
     return response;
   }
 
+  @Auth(true)
   @Delete(API_ENDPOINTS.NEWS_AND_EVENTS.DELETE_NEWS_OR_EVENTS)
   public async deleteNewsAndEvents(
+    @Req() request: AppRequest,
     @Param() payload: DeleteNewsOrEventsDto
   ): Promise<DeleteNewsEventsResponseDto> {
-    payload.deletedBy = '56cb91bdc3464f14678934ca'; // TODO get user id from token data
+    payload.deletedBy = request?.user?._id;
     const response = await firstValueFrom(
       this.superAdminService.send(
         RMQ_MESSAGES.NEWS_AND_EVENTS.DELETE_NEWS_OR_EVENTS,
