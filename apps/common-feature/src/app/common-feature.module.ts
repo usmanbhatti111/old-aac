03e128905b50f9edf5c11b12290e949b69684cfe
Khubaib <khubaib.rafique@ceative.co.uk>
2023-11-02 12:37:48 +0500

    fix(removed): commented out twilio calls (#AACSA-180)

--- apps/common-feature/src/app/common-feature.module.ts
+++ apps/common-feature/src/app/common-feature.module.ts
@@ -7,29 +7,29 @@ import { DocumentsController } from './controllers/documents.controller';
 import { DocumentsService } from './services/documents.service';
 import { AttachmentService } from './services/attachment.service';
 import { AttachmentController } from './controllers/attachment.controller';
-import { CallsController } from './controllers/calls.controller';
-import { CallsService } from './services/calls.service';
-import { TwilioModule } from 'nestjs-twilio';
+// import { CallsController } from './controllers/calls.controller';
+// import { CallsService } from './services/calls.service';
+// import { TwilioModule } from 'nestjs-twilio';
 
 @Module({
   imports: [
     SharedModule,
-    TwilioModule.forRoot({
-      accountSid: process.env.TWILIO_ACCOUNT_SID,
-      authToken: process.env.TWILIO_AUTH_TOKEN,
-    }),
+    // TwilioModule.forRoot({
+    //   accountSid: process.env.TWILIO_ACCOUNT_SID,
+    //   authToken: process.env.TWILIO_AUTH_TOKEN,
+    // }),
   ],
   controllers: [
     ContactController,
     DocumentsController,
     AttachmentController,
-    CallsController,
+    // CallsController,
   ],
   providers: [
     ContactService,
     DocumentsService,
     AttachmentService,
-    CallsService,
+    // CallsService,
   ],
 })
 export class CommonFeatureModule {}
