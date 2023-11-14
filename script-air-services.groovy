2106688c8a04f61c0d9d8dec0bec904bb2d450e3
Ali Ahmad <ali.ahmad@ceative.co.uk>
2023-11-14 11:03:13 +0500

    fix(Adding Jenkinsfiles): Adding all groovy files (#devops04)

--- script-air-services.groovy
+++ script-air-services.groovy
@@ -7,9 +7,8 @@ def deployImg(){
               sh """
                 ssh -o StrictHostKeyChecking=no -tt ubuntu@18.132.234.175 '
                   cd AAC-BE
-                  echo "Before git pull"
+                  npm i -f
                   git pull origin dev
-                  echo "After git pull"
                   docker compose -f docker-compose2.yml up -d --build air-services
                   docker system prune -a -f
                 '
