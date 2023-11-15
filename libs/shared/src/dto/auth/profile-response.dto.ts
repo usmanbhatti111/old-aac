e08d1968b42edac2e1711889bb3a0b7477ae2bf1
Attzaz Khan <muhammad.attzaz@ceative.co.uk>
2023-11-15 09:51:24 +0500

    feat(activitylog): Fixes in apis with merge (#AACSA-242)

--- libs/shared/src/dto/auth/profile-response.dto.ts
+++ libs/shared/src/dto/auth/profile-response.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class UserProfileResponseDto1 {
+export class UserProfileResponseDto {
   @ApiProperty({ example: 201 })
   statusCode: number;
 
