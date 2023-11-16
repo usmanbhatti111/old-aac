d82deba74a7049fefaea95985cb9748d52b04b21
Noor ul ain Khan <46588462+noor-nkn@users.noreply.github.com>
2023-11-17 03:59:17 +0500

    fix(org-company account) : updated pagination implementation (#AIR-554)

--- apps/org-admin/src/app/services/organization-company-account.service.ts
+++ apps/org-admin/src/app/services/organization-company-account.service.ts
@@ -54,16 +54,15 @@ export class OrganizationCompanyAccountService {
     payload: GetAllOrganizationCompanyAccountsDto
   ) {
     try {
-      const page = payload?.page || 1; // Default to page 1 if not specified
-      const perPage = payload?.limit || 10; // Default to 10 items per page if not specified
+      const filterQuery = {
+        isDeleted: false,
+        organizationId: new mongoose.Types.ObjectId(payload?.organizationId),
+      };
 
-      const skip = (page - 1) * perPage;
-
-      // Get the total count of records (without pagination)
-      const totalCount = await this.organizationCompanyAccountRepository.count({
-        organizationId: payload?.organizationId,
-      });
-
+      let { limit, page } = payload;
+      limit = limit ? limit : 10;
+      page = page ? page : 1;
+      const offset = limit * (page - 1);
       const pipelines: any = [
         {
           $lookup: {
@@ -74,21 +73,16 @@ export class OrganizationCompanyAccountService {
           },
         },
       ];
-
       const res = await this.organizationCompanyAccountRepository.paginate({
-        filterQuery: {
-          organizationId: new mongoose.Types.ObjectId(payload?.organizationId),
-        },
-        offset: skip,
-        limit: perPage,
+        filterQuery,
+        offset,
+        limit,
         pipelines,
       });
 
-      return successResponse(HttpStatus.OK, 'Success', res, {
-        count: totalCount,
-        page: page,
-        limit: perPage,
-      });
+      const response = successResponse(HttpStatus.OK, 'Success', res);
+
+      return response;
     } catch (error) {
       return errorResponse(HttpStatus.BAD_REQUEST, error?.meta?.message);
     }
